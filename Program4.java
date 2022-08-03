b0eb0b60ebcc7b9aa78470236f9c7bff2b056654
vaishnavi kane <109944617+vaishu009@users.noreply.github.com>
2022-08-03 22:34:54 +0530

    question 4

---
+++ Program4.java
@@ -0,0 +1,23 @@
+/*
+Write a program to print the area and perimeter of a triangle having sides of 3, 4 and 5 units by creating a class named 'Triangle' with 
+constructor having the three sides as its parameters.
+ */
+public class Program4 {
+     int s1,s2,s3;
+    public Program4(int side1,int side2,int side3)
+     {
+        s1=side1;
+        s2=side2;
+        s3=side3;
+     }
+       public static void main(String[] args)
+       {
+             program4 t1=new program4(3,4,5);
+             int peri=(t1.s1+t1.s2+t1.s3);
+             System.out.println("perimeter is:"+peri);
+             int sp=(peri/2);
+             int ar=((((sp*(sp-t1.s1))*(sp-t1.s2))*(sp-t1.s3)));       
+             int area=(int)Math.sqrt(ar);   
+             System.out.println("Area is:"+area);
+        }
+}
